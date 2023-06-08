c0e4d7d214ac726c98bedb0a8c84ef3495726717
jd479800 <jd479800@gmail.com>
2023-06-08 18:46:02 -0400

    Adds error message if email or password is wrong and formats code

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,64 +1,69 @@
-import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { Link } from 'react-router-dom';
-import { LOGIN } from '../utils/mutations';
-import Auth from '../utils/auth';
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { Link } from "react-router-dom";
+import { LOGIN } from "../utils/mutations";
+import Auth from "../utils/auth";
 
 function Login(props) {
-    const [formState, setFormState] = useState({ email: '', password: '' });
-    const [login, { error }] = useMutation(LOGIN);
+  const [formState, setFormState] = useState({ email: "", password: "" });
+  const [login, { error }] = useMutation(LOGIN);
 
-    const handleFormSubmit = async (event) => {
-        event.preventDefault();
-        try {
-          const mutationResponse = await login({
-            variables: { email: formState.email, password: formState.password },
-          });
-          const token = mutationResponse.data.login.token;
-          Auth.login(token);
-        } catch (e) {
-          console.log(e);
-        }
-      };
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const mutationResponse = await login({
+        variables: { email: formState.email, password: formState.password },
+      });
+      const token = mutationResponse.data.login.token;
+      Auth.login(token);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-      const handleChange = (event) => {
-        const { name, value } = event.target;
-        setFormState({
-          ...formState,
-          [name]: value,
-        });
-      };
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
 
-      return (
-        <div className="container my-1">
-          <Link to="/signup">Signup</Link>
-    
-          <h2>Login</h2>
-          <form onSubmit={handleFormSubmit}>
-            <div>
-              <label htmlFor="email">Email:</label>
-              <input
-                placeholder="Enter email"
-                name="email"
-                type="email"
-                id="email"
-                onChange={handleChange}
-              />
-            </div>
-            <div>
-              <label htmlFor="pwd">Password:</label>
-              <input
-                placeholder="*********"
-                name="password"
-                type="password"
-                id="pwd"
-                onChange={handleChange}
-              />
-            </div>
-            <div>
-              <button type="submit">Login</button>
-            </div>
-          </form>
+  return (
+    <div className="container my-1">
+      <Link to="/signup">Signup</Link>
+
+      <h2>Login</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            placeholder="Enter email"
+            name="email"
+            type="email"
+            id="email"
+            onChange={handleChange}
+          />
         </div>
-      );
+        <div>
+          <label htmlFor="pwd">Password:</label>
+          <input
+            placeholder="*********"
+            name="password"
+            type="password"
+            id="pwd"
+            onChange={handleChange}
+          />
+        </div>
+        {error ? (
+          <div>
+            <p className="error-text">The provided credentials are incorrect</p>
+          </div>
+        ) : null}
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  );
 }
