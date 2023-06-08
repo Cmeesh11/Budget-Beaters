777f67370ca726b483b487a0e91d5f9e5a28ecca
jd479800 <jd479800@gmail.com>
2023-06-08 18:40:29 -0400

    Adds handle for login form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,3 +3,21 @@ import { useMutation } from '@apollo/client';
 import { Link } from 'react-router-dom';
 import { LOGIN } from '../utils/mutations';
 import Auth from '../utils/auth';
+
+function Login(props) {
+    const [formState, setFormState] = useState({ email: '', password: '' });
+    const [login, { error }] = useMutation(LOGIN);
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        try {
+          const mutationResponse = await login({
+            variables: { email: formState.email, password: formState.password },
+          });
+          const token = mutationResponse.data.login.token;
+          Auth.login(token);
+        } catch (e) {
+          console.log(e);
+        }
+      };
+}
