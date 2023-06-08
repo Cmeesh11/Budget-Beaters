26d0967e737bf8e4daebb739f30802e8b03fbb18
Nate Long <nate.k.long@gmail.com>
2023-06-08 18:49:12 -0400

    Added CarDetails.js, NoMatch.js, added some HTML to Home.js

---
+++ client/src/pages/NoMatch.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+const NoMatch = () => {
+  return (
+    <div>
+
+        <h1>No results found!</h1>
+        <h1>
+          <span role="img" aria-label="Face With Rolling Eyes Emoji">
+            🙄
+          </span>
+        </h1>
+
+    </div>
+  );
+};
+
+export default NoMatch;
