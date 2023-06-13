4a68aca2feac92312626dd15624e3abc66431209
Cmeesh11 <cartermeesh11@gmail.com>
2023-06-12 20:09:20 -0400

    created mutations and queries files

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -2,15 +2,15 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
-import { ADD_USER } from '../utils/mutations';
+import { CREATE_USER } from '../utils/mutations';
 
 function Signup(props) {
     const [formState, setFormState] = useState({ email: '', password: '' });
-    const [addUser] = useMutation(ADD_USER);
+    const [createUser] = useMutation(CREATE_USER);
   
     const handleFormSubmit = async (event) => {
       event.preventDefault();
-      const mutationResponse = await addUser({
+      const mutationResponse = await createUser({
         variables: {
           email: formState.email,
           password: formState.password,
@@ -18,7 +18,7 @@ function Signup(props) {
           lastName: formState.lastName,
         },
       });
-      const token = mutationResponse.data.addUser.token;
+      const token = mutationResponse.data.createUser.token;
       Auth.login(token);
     };
   
